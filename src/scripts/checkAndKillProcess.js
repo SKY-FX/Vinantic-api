de01ea5abb62d700802fc938ed3b7b682c53d4e5
Chabaud <dsp_fx@hotmail.com>
2023-07-11 00:13:28 +0200

    change in interval

--- src/scripts/checkAndKillProcess.js
+++ src/scripts/checkAndKillProcess.js
@@ -1,8 +1,8 @@
 const { exec } = require("child_process");
 
-const DELAY_BEFORE_KILL = 20000;
+const DELAY_BEFORE_KILL = 3600000; // 1 hour in milliseconds
 
-/* Kill the processes whose ports are at 4000 and the states are CLOSE_WAIT */
+/* Check and Kill the processes whose ports are at 4000 and the states are CLOSE_WAIT */
 const checkAndKillProcess = () => {
   exec(
     "pids=$(lsof -i tcp:4000 | grep CLOSE_WAIT | awk '{print $2}'); for pid in $pids; do echo \"Killing process with PID $pid\"; kill -9 $pid; done",
@@ -16,8 +16,7 @@ const checkAndKillProcess = () => {
   );
 };
 
-/* Wait for DELAY_BEFORE_KILL before killing the processes */
-/* CLOSE_WAIT processes do not immediately appear in the process list */
-const checkAndKillProcessAfterDelay = () => setTimeout(checkAndKillProcess, DELAY_BEFORE_KILL);
+/* Do it every DELAY_BEFORE_KILL seconds */
+const checkAndKillProcessAfterDelay = () => setInterval(checkAndKillProcess, DELAY_BEFORE_KILL);
 
 module.exports = { checkAndKillProcessAfterDelay };
